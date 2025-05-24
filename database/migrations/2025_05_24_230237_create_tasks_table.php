b5f8f913047e34cf850d269264eae91f72e26935
khaoua maryem <maryem.khaoua@gmail.com>
2025-05-25 00:04:15 +0100

    make migration

--- database/migrations/2025_05_24_230237_create_tasks_table.php
+++ database/migrations/2025_05_24_230237_create_tasks_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['in_progress', 'done'])->default('in_progress');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 };
