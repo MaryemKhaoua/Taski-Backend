9da4f2b6d7f35fca28cdf7875b150db416083409
khaoua maryem <maryem.khaoua@gmail.com>
2025-05-25 18:14:43 +0100

    Bind repository interfaces to implementations in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+use App\Repositories\UserRepository;
+use App\Repositories\Interfaces\TaskRepositoryInterface;
+use App\Repositories\TaskRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,8 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(TaskRepositoryInterface::class, TaskRepository::class);
     }
+
 
     /**
      * Bootstrap any application services.
