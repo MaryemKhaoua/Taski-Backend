c4b580bcd34adaada97808f62663528ff4e19979
khaoua maryem <maryem.khaoua@gmail.com>
2025-05-25 17:19:39 +0100

    add some modification

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -6,24 +6,26 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class LoginRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
             'username' => 'required|string',
             'password' => 'required|string',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'username.required' => 'Username is required.',
+            'username.string' => 'Username must be a valid string.',
+            'password.required' => 'Password is required.',
+            'password.string' => 'Password must be a valid string.',
         ];
     }
 }
