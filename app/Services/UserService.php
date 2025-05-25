a25fa4fc5da69391be0a11f531f7c71228a592d9
khaoua maryem <maryem.khaoua@gmail.com>
2025-05-25 18:11:30 +0100

    user service

---
+++ app/Services/UserService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Interfaces\UserRepositoryInterface;
+
+class UserService
+{
+    protected $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function getAllUsers()
+    {
+        return $this->userRepository->all();
+    }
+}
