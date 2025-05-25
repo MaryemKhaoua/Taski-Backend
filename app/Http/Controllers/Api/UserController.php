fe6211eb560709d5b3a6d814f5c88f4187623107
khaoua maryem <maryem.khaoua@gmail.com>
2025-05-25 18:12:11 +0100

    fetch all users and handel empty user

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Services\UserService;
+use Illuminate\Http\JsonResponse;
+
+class UserController extends Controller
+{
+    protected $userService;
+
+    public function __construct(UserService $userService) {
+        $this->userService = $userService;
+    }
+
+    public function index(): JsonResponse {
+        $users = $this->userService->getAllUsers();
+        return response()->json($users);
+    }
+}
