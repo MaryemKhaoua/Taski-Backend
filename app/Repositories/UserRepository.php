b0426384f52470cf8c050795e78c9eac557735e4
khaoua maryem <maryem.khaoua@gmail.com>
2025-05-26 18:52:10 +0100

    restrict user listing to admins and improve error handling

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -8,14 +8,7 @@ use App\Repositories\Interfaces\UserRepositoryInterface;
 class UserRepository implements UserRepositoryInterface
 {
     public function all() {
-        $users = User::all();
-        
-        if ($users->isEmpty()) {
-            return response()->json([
-                'message' => 'No users found',
-            ], 404);
-        }
-        
-        return $users;
+        return User::all();
     }
+
 }
